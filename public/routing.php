54510f55fe6cc3d01f7f71b2eaf30b719accfd33
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-08-11 20:57:10 +0200

    set apache document root public and fix .htaccess

--- public/routing.php
+++ public/routing.php
@@ -4,18 +4,15 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
     '/' => '/../src/views/itineraries/add.php',
-    '/public/index.php' => '../src/views/itineraries/add.html',
+    '/ciao' => '/../src/views/itineraries/add.php',
 ];
 
 if (array_key_exists($uri, $routes)) {
-    echo 'ciao 2';
-   include __DIR__ . $routes[$uri];
+    echo 'YES';
+    include __DIR__ . $routes[$uri];
     echo $routes[$uri];
 }
 
-
-include __DIR__ . "/../src/views/itineraries/add.php";
 ?>
 
-
-<p>ciao</p>
+<p>ROUTING</p>
