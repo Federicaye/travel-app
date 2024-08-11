7e2ee931c4af029ca54e2cf9f11539651588b8f4
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-08-11 11:03:01 +0200

    model

--- src/models/itinerary.php
+++ src/models/itinerary.php
@@ -1,25 +1,55 @@
 <?php
-//including the database connection file
+
 include $_SERVER['DOCUMENT_ROOT'] . '/travel-app/db/connection.php';
 
-//fetching data in descending order (lastest entry first)
 
-class Trip {
-    
+class Trip
+{
+
 
     private static $conn;
 
-    public static function setConnection() {
-       self::$conn = Database::getConnection();
+    public static function setConnection()
+    {
+        self::$conn = Database::getConnection();
     }
-    public static function index() {
+    public static function index()
+    {
         self::setConnection();
         $trips = self::$conn->query("SELECT * FROM trips");
         return $trips->fetch_all(MYSQLI_ASSOC);
     }
-
-    public function show() {
-        $trips = self::$conn->query("SELECT * FROM trips");
+    public static function show($id)
+    {
+        $id = $_GET['id'];
+        self::setConnection();
+        $trips = self::$conn->query("SELECT * FROM trips WHERE id = $id");
         return $trips->fetch_all();
+    }
+
+    public static function update($id)
+    {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $id = $_GET['id'];
+        $sql = "UPDATE trips 
+        SET title='$title', 
+        description='$description',
+        image= 'ciao',
+        WHERE id=2";
+        self::setConnection();
+        $trips = self::$conn->query($sql);
+    }
+
+    public static function delete($id)
+    {
+        $id = $_GET['id'];
+        self::setConnection();
+    }
+
+    public static function store($id)
+    {
+        $id = $_GET['id'];
+        self::setConnection();
     }
 }
