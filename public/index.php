79de6b4a7a7681a87de0d9175d8f136d79b99576
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-08-14 13:48:05 +0200

    isset query

--- public/index.php
+++ public/index.php
@@ -4,14 +4,18 @@ use GuzzleHttp\Psr7\Query;
 require '../routing/Router.php';
 $router = new Router();
 require '../routing/routes.php'; 
+$url = parse_url($_SERVER['REQUEST_URI']);
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-$parameters = parse_url($_SERVER['REQUEST_URI'])['query'];
+
+$parameters = isset($url['query']) ? $url['query'] : null;
+
+
 $method = $_SERVER['REQUEST_METHOD'];
 $router->route($uri, $method, $parameters);
 
-/* var_dump(parse_url($_SERVER['REQUEST_URI'])['query']); */
+/* var_dump(parse_url($_SERVER['REQUEST_URI'])['query']); 
 var_dump($uri);
-var_dump($parameters);
+var_dump($parameters); */ 
 
 
 
